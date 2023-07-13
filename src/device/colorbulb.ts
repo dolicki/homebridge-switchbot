3b0f94a34c055992a780bd466dc0a097e34e8d96
Goran Dolički <gorandolicki@gmail.com>
2023-07-13 22:07:51 +0200

    new build

--- src/device/colorbulb.ts
+++ src/device/colorbulb.ts
@@ -169,56 +169,56 @@ export class ColorBulb {
       .onSet(this.BrightnessSet.bind(this));
 
     // handle ColorTemperature events using the ColorTemperature characteristic
-    this.lightBulbService
-      .getCharacteristic(this.platform.Characteristic.ColorTemperature)
-      .setProps({
-        minValue: 140,
-        maxValue: 500,
-        validValueRanges: [140, 500],
-      })
-      .onGet(() => {
-        return this.ColorTemperature!;
-      })
-      .onSet(this.ColorTemperatureSet.bind(this));
+    // this.lightBulbService
+    //   .getCharacteristic(this.platform.Characteristic.ColorTemperature)
+    //   .setProps({
+    //     minValue: 140,
+    //     maxValue: 500,
+    //     validValueRanges: [140, 500],
+    //   })
+    //   .onGet(() => {
+    //     return this.ColorTemperature!;
+    //   })
+    //   .onSet(this.ColorTemperatureSet.bind(this));
 
     // handle Hue events using the Hue characteristic
-    this.lightBulbService
-      .getCharacteristic(this.platform.Characteristic.Hue)
-      .setProps({
-        minValue: 0,
-        maxValue: 360,
-        validValueRanges: [0, 360],
-      })
-      .onGet(() => {
-        return this.Hue;
-      })
-      .onSet(this.HueSet.bind(this));
+    // this.lightBulbService
+    //   .getCharacteristic(this.platform.Characteristic.Hue)
+    //   .setProps({
+    //     minValue: 0,
+    //     maxValue: 360,
+    //     validValueRanges: [0, 360],
+    //   })
+    //   .onGet(() => {
+    //     return this.Hue;
+    //   })
+    //   .onSet(this.HueSet.bind(this));
     // handle Hue events using the Hue characteristic
 
-    this.lightBulbService
-      .getCharacteristic(this.platform.Characteristic.Saturation)
-      .setProps({
-        minValue: 0,
-        maxValue: 100,
-        validValueRanges: [0, 100],
-      })
-      .onGet(() => {
-        return this.Saturation;
-      })
-      .onSet(this.SaturationSet.bind(this));
+    // this.lightBulbService
+    //   .getCharacteristic(this.platform.Characteristic.Saturation)
+    //   .setProps({
+    //     minValue: 0,
+    //     maxValue: 100,
+    //     validValueRanges: [0, 100],
+    //   })
+    //   .onGet(() => {
+    //     return this.Saturation;
+    //   })
+    //   .onSet(this.SaturationSet.bind(this));
 
-    // this.debugLog(`${this.device.deviceType}: ${this.accessory.displayName} adaptiveLightingShift: ${this.adaptiveLightingShift}`);
-    // if (this.adaptiveLightingShift !== -1) {
-    //   this.AdaptiveLightingController = new platform.api.hap.AdaptiveLightingController(this.lightBulbService, {
-    //     customTemperatureAdjustment: this.adaptiveLightingShift,
-    //   });
-    //   this.accessory.configureController(this.AdaptiveLightingController);
-    //   this.accessory.context.adaptiveLighting = true;
-    //   this.debugLog(
-    //     `${this.device.deviceType}: ${this.accessory.displayName} adaptiveLighting: ${this.accessory.context.adaptiveLighting},` +
-    //       ` adaptiveLightingShift: ${this.adaptiveLightingShift}`,
-    //   );
-    // }
+    this.debugLog(`${this.device.deviceType}: ${this.accessory.displayName} adaptiveLightingShift: ${this.adaptiveLightingShift}`);
+    if (this.adaptiveLightingShift !== -1) {
+      this.AdaptiveLightingController = new platform.api.hap.AdaptiveLightingController(this.lightBulbService, {
+        customTemperatureAdjustment: this.adaptiveLightingShift,
+      });
+      this.accessory.configureController(this.AdaptiveLightingController);
+      this.accessory.context.adaptiveLighting = true;
+      this.debugLog(
+        `${this.device.deviceType}: ${this.accessory.displayName} adaptiveLighting: ${this.accessory.context.adaptiveLighting},` +
+          ` adaptiveLightingShift: ${this.adaptiveLightingShift}`,
+      );
+    }
   }
 
   /**
